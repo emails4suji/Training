8c331757bf38347d25bc55648550273131799aae
Suji <emails4suji@gmail.com>
2016-07-24 18:45:17 +0530

    src code modified

--- src/pack/HelloWorld.java
+++ src/pack/HelloWorld.java
@@ -3,6 +3,6 @@ package pack;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi This program is compiled and run using ANT..!");
-	System.out.println("After cloning..edited and pushed");
+	System.out.println("Source code modified..!");
     }
 }
