c1bf33d6c9a770c4cd4e73f27e66a3a7dc97cbde
Suji <emails4suji@gmail.com>
2016-07-24 18:18:54 +0530

    Source code modified for testing

--- src/pack/HelloWorld.java
+++ src/pack/HelloWorld.java
@@ -3,6 +3,6 @@ package pack;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi This program is compiled and run using ANT..!");
-	System.out.println("Code pushed to github and build taken using travis ci");
+	System.out.println("After cloning..edited and pushed");
     }
 }
